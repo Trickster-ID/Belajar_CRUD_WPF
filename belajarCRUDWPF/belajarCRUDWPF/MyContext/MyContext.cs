2a2cce5c5c5b2d3fcba4c0fc92e4527204501fa6
Trickster-ID <pikritaufanaziz@gmail.com>
2020-03-19 14:37:40 +0700

    add model supplier

---
+++ belajarCRUDWPF/belajarCRUDWPF/MyContext/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using belajarCRUDWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace belajarCRUDWPF.MyContext
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("BelajarCRUDWPF") { }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
